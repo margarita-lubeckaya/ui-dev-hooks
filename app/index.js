d27f36693e7d5bbe425871a0f7a32209409bdccf
margarita.lubeckaya <margarita.lubeckaya@gmail.com>
2022-10-18 12:43:07 +0300

    update index.js

--- app/index.js
+++ app/index.js
@@ -1,49 +1,56 @@
 import React from 'react'
+import {useState, useMemo} from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import { ThemeProvider } from './contexts/theme'
+import {ThemeProvider} from './contexts/theme'
 import Nav from './components/Nav'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Loading from './components/Loading'
 
 const Popular = React.lazy(() => import('./components/Popular'))
 const Battle = React.lazy(() => import('./components/Battle'))
 const Results = React.lazy(() => import('./components/Results'))
 
-class App extends React.Component {
-  state = {
-    theme: 'light',
-    toggleTheme: () => {
-      this.setState(({ theme }) => ({
-        theme: theme === 'light' ? 'dark' : 'light'
-      }))
-    }
-  }
-  render() {
-    return (
-      <Router>
-        <ThemeProvider value={this.state}>
-          <div className={this.state.theme}>
-            <div className='container'>
-              <Nav />
+function App() {
 
-              <React.Suspense fallback={<Loading />} >
-                <Switch>
-                  <Route exact path='/' component={Popular} />
-                  <Route exact path='/battle' component={Battle} />
-                  <Route path='/battle/results' component={Results} />
-                  <Route render={() => <h1>404</h1>} />
-                </Switch>
-              </React.Suspense>
-            </div>
-          </div>
-        </ThemeProvider>
-      </Router>
+    const [theme, setTheme] = useState('light')
+
+    const themeContext = useMemo(() => {
+        return {
+            theme,
+            toggleTheme: () => {
+                setTheme((prev) => {
+                        return prev === 'light' ? 'dark' : 'light'
+                    }
+                )
+            }
+        }
+
+    }, [theme])
+
+
+    return (
+        <Router>
+            <ThemeProvider value={themeContext}>
+                <div className={theme}>
+                    <div className='container'>
+                        <Nav/>
+                        <React.Suspense fallback={<Loading/>}>
+                            <Switch>
+                                <Route exact path='/' component={Popular}/>
+                                <Route exact path='/battle' component={Battle}/>
+                                <Route path='/battle/results' component={Results}/>
+                                <Route render={() => <h1>404</h1>}/>
+                            </Switch>
+                        </React.Suspense>
+                    </div>
+                </div>
+            </ThemeProvider>
+        </Router>
     )
-  }
 }
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('app')
+    <App/>,
+    document.getElementById('app')
 )
